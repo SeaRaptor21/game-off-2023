89b768bc50e769bb9db2efbc25ee25a8792eb9d2
SeaRaptor21 <elschuette21@gmail.com>
2023-11-09 20:07:44 -0800

    Add assets and refine gravity

--- scripts/planet.gd
+++ scripts/planet.gd
@@ -1,11 +1,28 @@
-extends StaticBody2D
+extends Node2D
 
 var density = 5515.0
 var radius = 6.371e6
 var mass = density * 4/3*PI*radius**3
 
+const possible_images = [1,2,3,4,5,6,7,9,10,11,12,13,14,15,16,17,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,37,38,40,41]
+
 func _ready():
-	$CollisionShape2D.shape.radius = radius/Game.scale
-	var image_scale = radius/Game.scale/156
+	$Area2D/CollisionShape2D.shape.radius = radius/Game.scale
+	var image_scale = radius/Game.scale/(322/2)
+	$Sprite2D.texture = load('res://assets/planets/%d.png' % possible_images.pick_random())
 	$Sprite2D.scale.x = image_scale
 	$Sprite2D.scale.y = image_scale
+
+func scale_up(factor):
+	radius = radius*factor
+	mass = density * 4/3*PI*radius**3
+	$Area2D/CollisionShape2D.shape.radius = radius/Game.scale
+	var image_scale = radius/Game.scale/(322/2)
+	$Sprite2D.scale.x = image_scale
+	$Sprite2D.scale.y = image_scale
+	
+func scale_add(num):
+	scale_up((radius+num)/radius)
+
+func _on_area_2d_body_entered(body):
+	print('You die!!!!')
--- scripts/spaceship.gd
+++ scripts/spaceship.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 const mass = 74842.741
 @onready var planets = get_node("../Planets")
+@onready var bg = get_node("../Background/Layer")
 
 func _physics_process(delta):
 	for planet in planets.get_children():
@@ -9,4 +10,6 @@ func _physics_process(delta):
 		var force = (mass*planet.mass)/distance
 		var acc = force / mass
 		velocity += -acc*((position-planet.position).normalized())*delta*1e-12
+	rotation = velocity.angle()
+	#bg.motion_offset += velocity*delta*0.01
 	move_and_slide()
